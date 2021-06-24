9a758e8c65dceb25df701b4f57f4105f113448df
unknown <alinyaraujoufal@gmail.com>
2021-06-23 21:42:24 -0300

    login controller

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nlw_pay_flow/modules/login/login_controller.dart';
 import 'package:nlw_pay_flow/shared/themes/app_colors.dart';
 import 'package:nlw_pay_flow/shared/themes/app_images.dart';
 import 'package:nlw_pay_flow/shared/widgets/social_login/social_login_button.dart';
@@ -12,6 +13,7 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final controller = LoginController();
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -59,7 +61,7 @@ class _LoginPageState extends State<LoginPage> {
                         const EdgeInsets.only(left: 40, right: 40, top: 40),
                     child: SocialLoginButton(
                       onTap: () {
-                        print("clicou");
+                        controller.googleSignIn(context);
                       },
                     ),
                   )
